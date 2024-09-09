008a60d43ef80fc619c5b32426f56ffedda458a9
“FarazArshad1” <newsletters833@gmail.com>
2024-09-09 18:27:45 +0500

    Setup Dynamic URLs

---
+++ Dynamic URL, Redirection, URL Building/dynamic_urls.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+# create app
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return '<h1>welcome to the home page</h1>'
+
+@app.route("/welcome/steve")
+def welcome_steve():
+    return '<h1>Welcome Steve'
+
+@app.route('/welcome/<user>')
+def welcome_tony(user): 
+    return f'<h1>Welcome {user}</h1>'
+
+if __name__ == "__main__":
+    app.run(debug=True)
