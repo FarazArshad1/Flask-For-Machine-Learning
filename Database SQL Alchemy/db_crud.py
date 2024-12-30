91452ec89cf6240507231b3dce4facfe0ccdeb67
FarazArshad1 <newsletters833@gmail.com>
2024-12-30 13:55:45 +0500

    Implement Flask session

--- Database SQL Alchemy/db_crud.py
+++ Database SQL Alchemy/db_crud.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///employees.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #This will remove the warning which came while running
 
 db = SQLAlchemy(app)
 
